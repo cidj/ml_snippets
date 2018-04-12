4ac3fdea58712b13ab9484cc4af9e317e6953ffd
Tao <uku.ele@gmail.com>
2018-04-12 14:31:05 +0800

    Added HDBSCANClusterClassifier

--- codes_collected_sklearn.py
+++ codes_collected_sklearn.py
@@ -10,6 +10,52 @@ import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin, ClassifierMixin
 from sklearn.metrics.pairwise import paired_distances 
+import hdbscan
+
+#ClusterClassifier Framework, requires fit and predict method.
+class ClusterClassifier(BaseEstimator, ClassifierMixin):
+
+    def __init__(self, clustering_model=None):
+        self.clustering_model = clustering_model
+        self.comparison_summary=None
+
+
+    def fit(self, attributes0, label0):      
+        attributes=np.array(attributes0)
+        label=np.array(label0)
+        
+        self.clustering_model.fit(attributes)
+        pred=self.clustering_model.predict(attributes)
+
+        lab=pd.Series(label,name='lab',dtype=int)
+        pre=pd.Series(pred,name='pre',dtype=int)
+        kmcomp=pd.concat([lab,pre],axis=1)
+        
+        kmc1=kmcomp[kmcomp['lab']==1]
+        kmc0=kmcomp[kmcomp['lab']==0]
+        
+        sta1=kmc1['pre'].groupby(kmc1['pre']).count()
+        sta0=kmc0['pre'].groupby(kmc0['pre']).count()        
+        
+        align=pd.concat([sta1/len(kmc1),sta0/len(kmc0)],axis=1).fillna(0)
+        align.columns=['sta1a','sta0a']
+        
+        sta1a=align['sta1a']
+        sta0a=align['sta0a']
+        
+        dif=sta1a/(sta1a+sta0a)
+        result=pd.concat([pd.concat([sta1,sta0,sta1a,sta0a],axis=1).fillna(0),
+                          dif],axis=1)
+        result.columns=['sta1','sta0','sta1a','sta0a','dif']
+        resee=result.sort_values('dif',ascending=False)
+        resee['cumsta1a']=np.cumsum(resee['sta1a'])
+        resee['cumsta0a']=np.cumsum(resee['sta0a'])
+        resee['cumsta1']=np.cumsum(resee['sta1'])
+        resee['cumsta0']=np.cumsum(resee['sta0'])        
+      
+        self.comparison_summary=resee
+        
+        
 class KmeansClusterClassifier(BaseEstimator, ClassifierMixin):
 
     def __init__(self, clustering_model=None):
@@ -80,7 +126,7 @@ class KmeansClusterClassifier(BaseEstimator, ClassifierMixin):
             return ret
         
         
-class ClusterClassifier(BaseEstimator, ClassifierMixin):
+class HDBSCANClusterClassifier(BaseEstimator, ClassifierMixin):
 
     def __init__(self, clustering_model=None):
         self.clustering_model = clustering_model
@@ -130,8 +176,9 @@ class ClusterClassifier(BaseEstimator, ClassifierMixin):
 
         pick_clusters=self.comparison_summary[self.comparison_summary['dif']>=thresh].index.tolist()
         
-        res=self.clustering_model.predict(attributes)
-        return pd.Series(res).isin(pick_clusters).astype(int).values
+        res,cluster_strengths=self.clustering_model.approximate_predict(self.clustering_model, attributes)
+        
+        return (pd.Series(res).isin(pick_clusters).astype(int).values,cluster_strengths)
         
       
         
